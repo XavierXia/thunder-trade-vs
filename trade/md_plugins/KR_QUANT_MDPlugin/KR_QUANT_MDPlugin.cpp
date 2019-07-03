42617134396760904c35ba8d825714872415d5c3
XavierXia <xiawenxing2010@163.com>
2019-07-03 18:14:23 +0800

    update

--- trade/md_plugins/KR_QUANT_MDPlugin/KR_QUANT_MDPlugin.cpp
+++ trade/md_plugins/KR_QUANT_MDPlugin/KR_QUANT_MDPlugin.cpp
@@ -97,8 +97,10 @@ void CKrQuantMDPluginImp::MDInit(const ptree & in)
     // Start();
 
 	m_StartAndStopCtrlTimer.expires_from_now(time_duration(0,0,3,0));
-	m_StartAndStopCtrlTimer.async_wait(boost::bind(&CKrQuantMDPluginImp::TimerHandler,this));
-	m_futTimerThreadFuture=std::async([this] {
+	m_StartAndStopCtrlTimer.async_wait(boost::bind(&CKrQuantMDPluginImp::TimerHandler,this,
+		&m_StartAndStopCtrlTimer,
+		boost::asio::placeholders::error));
+	m_futTimerThreadFuture=std::async(std::launch::async,[this] {
 		this->m_IOservice.run();
 		return true;
 	});
