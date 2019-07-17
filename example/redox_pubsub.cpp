e6be75069f18cf53e0032743f0a448251feb7488
XavierXia <xiawenxing2010@163.com>
2019-07-17 10:38:13 +0800

    example update

--- example/redox_pubsub.cpp
+++ example/redox_pubsub.cpp
@@ -7,6 +7,9 @@ using namespace std;
 
 /*
 说明：本demo主要作用是 说明Zpquant与三方策略通信的数据格式实现细节
+编译方式：
+g++ redox_pubsub.cpp -o hello -std=c++11 -lredox -lev -lhiredis
+./hello
 */
 int main(int argc, char *argv[]) {
 
@@ -38,33 +41,41 @@ int main(int argc, char *argv[]) {
             case 20: //查询 客户端总览信息
             {
               str = "{\"type\":\"query\",\"category\":\"clientOverview\",\"code\":\"\",\"sclb\":\"\"}";
+              cout << "...query...cnt: " << cnt << endl;
+              publisher.publish("order2server", str);
               break;
             }
             case 25: //查询 所有关联资金账户的资金信息
             {
               str = "{\"type\":\"query\",\"category\":\"cashAsset\",\"code\":\"\",\"sclb\":\"\"}";
+              cout << "...query...cnt: " << cnt << endl;
+              publisher.publish("order2server", str);
               break;
             }
             case 30: //查询 指定上证 600000 的产品信息
             {
               str = "{\"type\":\"query\",\"category\":\"stkInfo\",\"code\":\"600000\",\"sclb\":\"2\"}";
+              cout << "...query...cnt: " << cnt << endl;
+              publisher.publish("order2server", str);
               break;
             }
             case 35: //查询 上证 600000 股票的持仓
             {
               str = "{\"type\":\"query\",\"category\":\"stkHolding\",\"code\":\"600000\",\"sclb\":\"2\"}";
+              cout << "...query...cnt: " << cnt << endl;
+              publisher.publish("order2server", str);
               break;
             }
             case 40: //查询 沪深两市 所有股票持仓
             {
               str = "{\"type\":\"query\",\"category\":\"stkHolding\",\"code\":\"\",\"sclb\":\"\"}";
+              cout << "...query...cnt: " << cnt << endl;
+              publisher.publish("order2server", str);
               break;
             }
             default:
               break;
           }
-      	  cout << "...query" << endl;
-          publisher.publish("order2server", str);
       }
 
       if(cnt >= 80 && cnt < 121)
@@ -74,28 +85,34 @@ int main(int argc, char *argv[]) {
             case 80: //* 以 12.67元 购买 浦发银行(600000) 200股 */
             {
               str = "{\"type\":\"buy\",\"code\":\"600000\",\"sclb\":\"1\",\"wtfs\":\"0\",\"amount\":\"100\",\"price\":\"126700\"}";
+              cout << "...query...cnt: " << cnt << endl;
+              publisher.publish("order2server", str);
               break;
             }
             case 90: //* 以 市价 卖出  浦发银行(600000) 100股 */
             {
               str = "{\"type\":\"sell\",\"code\":\"600000\",\"sclb\":\"1\",\"wtfs\":\"1\",\"amount\":\"100\",\"price\":\"0\"}";
+              cout << "...query...cnt: " << cnt << endl;
+              publisher.publish("order2server", str);
               break;
             }
             case 100: ///* 以 12.67元 购买 平安银行(000001) 200股 */
             {
               str = "{\"type\":\"buy\",\"code\":\"000001\",\"sclb\":\"2\",\"wtfs\":\"0\",\"amount\":\"200\",\"price\":\"126700\"}";
+              cout << "...query...cnt: " << cnt << endl;
+              publisher.publish("order2server", str);
               break;
             }
             case 110: ///* 以 市价 卖出 平安银行(000001) 100股 */
             {
               str = "{\"type\":\"sell\",\"code\":\"000001\",\"sclb\":\"2\",\"wtfs\":\"1\",\"amount\":\"100\",\"price\":\"0\"}";
+              cout << "...query...cnt: " << cnt << endl;
+              publisher.publish("order2server", str);
               break;
             }
             default:
               break;
           }
-          cout << "...buy" << endl;
-          publisher.publish("order2server", str);
       }
 
       if(cnt == 160)
