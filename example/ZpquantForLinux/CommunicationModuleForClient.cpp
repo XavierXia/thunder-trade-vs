ef11ddf83ed6f8a663308a868b0d4b84514c2cc2
XavierXia <xiawenxing2010@163.com>
2019-07-30 16:53:25 +0800

    fix

--- example/ZpquantForLinux/CommunicationModuleForClient.cpp
+++ example/ZpquantForLinux/CommunicationModuleForClient.cpp
@@ -39,14 +39,18 @@ void Communicate(const char * address, unsigned int port,const std::stringstream
         io_service service;
         boost::asio::ip::tcp::socket sock_(service);
         sock_.connect(ep);
+
         size_t PacketLength = in.str().size() + sizeof(int32_t);           
         // std::unique_ptr<char[]> sendbuf(new char[PacketLength]);
         // *((size_t*)sendbuf.get()) = PacketLength;
         // strncpy(sendbuf.get() + sizeof(int32_t), in.str().c_str(), in.str().size());
         // sock_.write_some(buffer(sendbuf.get(), PacketLength));
-        char sendbuf[PacketLength];
-        strncpy(sendbuf + sizeof(int32_t), in.str().c_str(), in.str().size());
-        sock_.write_some(buffer(sendbuf, PacketLength));
+
+        // char sendbuf[PacketLength];
+        // strncpy(sendbuf + sizeof(int32_t), in.str().c_str(), in.str().size());
+        // sock_.write_some(buffer(sendbuf, PacketLength));
+
+        sock_.write_some(buffer(in.str().c_str(), PacketLength));
         auto rcvlen = read(
             sock_,
             buffer(recvbuf),
@@ -60,3 +64,29 @@ void Communicate(const char * address, unsigned int port,const std::stringstream
         cout << err.what() << endl;
     }
 }
+
+void Communicate1(const char * address, unsigned int port,const std::stringstream & in, std::stringstream & out)
+{
+
+    // // 所有asio类都需要io_service对象
+    // io_service iosev;
+    // // socket对象
+    // ip::tcp::socket socket(iosev);
+    // // 连接端点，这里使用了本机连接，可以修改IP地址测试远程连接
+    // ip::tcp::endpoint ep(ip::address_v4::from_string(address), port);
+    // // 连接服务器
+    // boost::system::error_code ec;
+    // socket.connect(ep, ec);
+    // // 如果出错，打印出错信息
+    // if (ec)
+    // {
+    //     std::cout << boost::system::system_error(ec).what() << std::endl;
+    //     return -1;
+    // }
+    // // 接收数据
+    // char buf[MAX_ASIO_READ_BUFFER_LENGTH];
+    // size_t len = socket.read_some(buffer(buf), ec);
+    // std::cout.write(buf, len);
+}
+
+
