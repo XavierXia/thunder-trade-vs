b6ddfc727d6301812b972eb1394be974c855f4cc
XavierXia <xiawenxing2010@163.com>
2019-07-30 15:54:46 +0800

    fix

--- example/ZpquantForLinux/CommunicationModuleForClient.cpp
+++ example/ZpquantForLinux/CommunicationModuleForClient.cpp
@@ -41,10 +41,12 @@ void Communicate(const char * address, unsigned int port,const std::stringstream
         sock_.connect(ep);
         size_t PacketLength = in.str().size() + sizeof(int32_t);           
         // std::unique_ptr<char[]> sendbuf(new char[PacketLength]);
+        // *((size_t*)sendbuf.get()) = PacketLength;
+        // strncpy(sendbuf.get() + sizeof(int32_t), in.str().c_str(), in.str().size());
+        // sock_.write_some(buffer(sendbuf.get(), PacketLength));
         char sendbuf[PacketLength];
-        *((size_t*)sendbuf.get()) = PacketLength;
-        strncpy(sendbuf.get() + sizeof(int32_t), in.str().c_str(), in.str().size());
-        sock_.write_some(buffer(sendbuf.get(), PacketLength));
+        strncpy(sendbuf + sizeof(int32_t), in.str().c_str(), in.str().size());
+        sock_.write_some(buffer(sendbuf, PacketLength));
         auto rcvlen = read(
             sock_,
             buffer(recvbuf),
