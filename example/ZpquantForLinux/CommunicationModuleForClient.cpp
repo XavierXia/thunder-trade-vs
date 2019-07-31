405640a98fd9596827a1e2b4f5f8fdb1a58c153e
XavierXia <xiawenxing2010@163.com>
2019-07-31 10:34:30 +0800

    fix

--- example/ZpquantForLinux/CommunicationModuleForClient.cpp
+++ example/ZpquantForLinux/CommunicationModuleForClient.cpp
@@ -42,11 +42,14 @@ void Communicate(const char * address, unsigned int port,const std::stringstream
         sock_.connect(ep);
 
         size_t PacketLength = in.str().size() + sizeof(int32_t);         
-        std::unique_ptr<char[]> sendbuf;
-        sendbuf.reset(new char[PacketLength]);
-        *((size_t*)sendbuf.get()) = PacketLength;
-        strncpy(sendbuf.get() + sizeof(int32_t), in.str().c_str(), in.str().size());
-        sock_.write_some(buffer(sendbuf.get(), PacketLength));
+        // std::unique_ptr<char[]> sendbuf(new char[PacketLength]);
+        // *((size_t*)sendbuf.get()) = PacketLength;
+        // strncpy(sendbuf.get() + sizeof(int32_t), in.str().c_str(), in.str().size());
+        // sock_.write_some(buffer(sendbuf.get(), PacketLength));
+
+        std::vector<char> sendbuf(PacketLength);
+        strncpy(sendbuf + sizeof(int32_t), in.str().c_str(), in.str().size());
+        sock_.write_some(buffer(sendbuf, PacketLength));
 
         // char sendbuf[PacketLength];
         // strncpy(sendbuf + sizeof(int32_t), in.str().c_str(), in.str().size());
