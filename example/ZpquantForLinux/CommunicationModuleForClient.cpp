02d1701b7433ad2e0da3985a2ee7bde5c944a79a
XavierXia <xiawenxing2010@163.com>
2019-07-31 11:17:49 +0800

    fix

--- example/ZpquantForLinux/CommunicationModuleForClient.cpp
+++ example/ZpquantForLinux/CommunicationModuleForClient.cpp
@@ -28,10 +28,10 @@ size_t ReadComplete(char * buf, size_t maxlen,const boost::system::error_code & 
     }
 }
 
-void Communicate(const char * address, unsigned int port,const std::stringstream & in, std::stringstream & out)
+void Communicate1(const char * address, unsigned int port,const std::stringstream & in, std::stringstream & out)
 {
     try {
-        int i = 0;
+
         using namespace boost::asio;
         if (in.str().size() >= MAX_ASIO_READ_BUFFER_LENGTH - sizeof(int32_t))
             throw std::runtime_error("The input string is too long.Is must shorter than MAX_ASIO_READ_BUFFER_LENGTH-sizeof(int32_t)");
@@ -42,14 +42,14 @@ void Communicate(const char * address, unsigned int port,const std::stringstream
         sock_.connect(ep);
 
         size_t PacketLength = in.str().size() + sizeof(int32_t);         
-        // std::unique_ptr<char[]> sendbuf(new char[PacketLength]);
-        // *((size_t*)sendbuf.get()) = PacketLength;
-        // strncpy(sendbuf.get() + sizeof(int32_t), in.str().c_str(), in.str().size());
-        // sock_.write_some(buffer(sendbuf.get(), PacketLength));
+        std::unique_ptr<char[]> sendbuf(new char[PacketLength]);
+        *((size_t*)sendbuf.get()) = PacketLength;
+        strncpy(sendbuf.get() + sizeof(int32_t), in.str().c_str(), in.str().size());
+        sock_.write_some(buffer(sendbuf.get(), PacketLength));
 
-        std::vector<char> sendbuf(PacketLength);
-        strncpy(&sendbuf.front(), in.str().c_str(), in.str().size());
-        sock_.write_some(buffer(sendbuf, PacketLength));
+        // std::vector<char> sendbuf(PacketLength);
+        // strncpy(&sendbuf.front(), in.str().c_str(), in.str().size());
+        // sock_.write_some(buffer(sendbuf, PacketLength));
 
         // char sendbuf[PacketLength];
         // strncpy(sendbuf + sizeof(int32_t), in.str().c_str(), in.str().size());
@@ -70,28 +70,41 @@ void Communicate(const char * address, unsigned int port,const std::stringstream
     }
 }
 
-void Communicate1(const char * address, unsigned int port,const std::stringstream & in, std::stringstream & out)
+void Communicate(const char * address, unsigned int port,const std::stringstream & in, std::stringstream & out)
 {
+    try {
+        using namespace boost::asio;
+        if (in.str().size() >= MAX_ASIO_READ_BUFFER_LENGTH - sizeof(int32_t))
+            throw std::runtime_error("The input string is too long.Is must shorter than MAX_ASIO_READ_BUFFER_LENGTH-sizeof(int32_t)");
+        
+        // 所有asio类都需要io_service对象
+        io_service iosev;
+        // socket对象
+        ip::tcp::socket socket(iosev);
+        // 连接端点，这里使用了本机连接，可以修改IP地址测试远程连接
+        ip::tcp::endpoint ep(ip::address::from_string(address), port);
+        // 连接服务器
+        boost::system::error_code ec;
+        socket.connect(ep, ec);
 
-    // // 所有asio类都需要io_service对象
-    // io_service iosev;
-    // // socket对象
-    // ip::tcp::socket socket(iosev);
-    // // 连接端点，这里使用了本机连接，可以修改IP地址测试远程连接
-    // ip::tcp::endpoint ep(ip::address_v4::from_string(address), port);
-    // // 连接服务器
-    // boost::system::error_code ec;
-    // socket.connect(ep, ec);
-    // // 如果出错，打印出错信息
-    // if (ec)
-    // {
-    //     std::cout << boost::system::system_error(ec).what() << std::endl;
-    //     return -1;
-    // }
-    // // 接收数据
-    // char buf[MAX_ASIO_READ_BUFFER_LENGTH];
-    // size_t len = socket.read_some(buffer(buf), ec);
-    // std::cout.write(buf, len);
+        char sendbuf[PacketLength];
+        strncpy(sendbuf + sizeof(int32_t), in.str().c_str(), in.str().size());
+        socket.write_some(buffer(sendbuf, PacketLength));
+
+        char recvbuf[MAX_ASIO_READ_BUFFER_LENGTH];
+        auto rcvlen = read(
+            socket,
+            buffer(recvbuf),
+            boost::bind(&ReadComplete, recvbuf, sizeof(recvbuf), _1, _2)
+            );
+        recvbuf[rcvlen] = 0;
+        out << recvbuf + sizeof(int32_t);
+
+    }
+    catch (std::exception & err)
+    {
+        cout << err.what() << endl;
+    }
 }
 
 
