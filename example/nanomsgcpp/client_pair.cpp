55fa0e9f0dc027d6a8158cf6defc3cdc6a48a4ce
XavierXia <xiawenxing2010@163.com>
2019-08-04 20:41:48 +0800

    fix

--- example/nanomsgcpp/client_pair.cpp
+++ example/nanomsgcpp/client_pair.cpp
@@ -5,10 +5,18 @@
 #include <iostream>
 using namespace std;
 
+#define ADDRESS1 "inproc://test"
+#define ADDRESS2 "tcp://127.0.0.1:8000"
+#define ADDRESS2 "ipc:///tmp/reqrep.ipc"
+
 int main ()
 {
     nn::socket s2 (AF_SP, NN_PAIR);
-    s2.connect ("inproc://a");
-    s2.send ("...ABC,client", 13, 0);
+    s2.connect (ADDRESS2);
+    
+    while(1)
+    {
+        s2.send ("...ABC,client", 13, 0);
+    }
     return 0;
 }
--- example/nanomsgcpp/server_pair.cpp
+++ example/nanomsgcpp/server_pair.cpp
@@ -4,19 +4,18 @@
 #include <cassert>
 #include <iostream>
 using namespace std;
+#define ADDRESS2 "ipc:///tmp/reqrep.ipc"
 
 int main ()
 {
     nn::socket s1 (AF_SP, NN_PAIR);
-    s1.bind ("inproc://a");
+    s1.bind (ADDRESS2);
 
     char buf [20];
-    int rc = s1.recv (buf, sizeof (buf), 0);
-    cout<<"...buf: " << buf << endl;
-
     while(1)
     {
-
+        int rc = s1.recv (buf, sizeof (buf), 0);
+        cout<<"...buf: " << buf << endl;
     };
     return 0;
 }
