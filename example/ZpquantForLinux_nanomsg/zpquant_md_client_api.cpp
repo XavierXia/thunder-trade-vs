69c13a8e0e2b52ac11da9ec5b714208c232606fd
XavierXia <xiawenxing2010@163.com>
2019-08-05 14:04:18 +0800

    fix

--- example/ZpquantForLinux_nanomsg/zpquant_md_client_api.cpp
+++ example/ZpquantForLinux_nanomsg/zpquant_md_client_api.cpp
@@ -9,17 +9,16 @@
 #define ADDRESS2 "tcp://*:8000"
 #define ADDRESS3 "ipc:///tmp/reqrep.ipc"
 
-nn::socket nnsocket(AF_SP, NN_PAIR);
-nnsocket.connect(ADDRESS2);
 
 void Communicate(const char * address, unsigned int port, const std::stringstream & in, std::stringstream & out);
 
 namespace Zpquant {
-
+    
+nn::socket nnsocket(AF_SP, NN_PAIR);
 
 CZpquantMdApi::CZpquantMdApi() {
     pSpi = NULL;
-    //nnsocket.connect(ADDRESS2);
+    nnsocket.connect(ADDRESS2);
     //memset(sendJsonDataStr, 0, sizeof(sendJsonDataStr)*4096);
 }
 
