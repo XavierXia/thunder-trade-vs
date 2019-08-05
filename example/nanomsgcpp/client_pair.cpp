a476791df75263f1ccca920c7cc4103ccb64fefd
XavierXia <xiawenxing2010@163.com>
2019-08-05 13:23:10 +0800

    fix

--- example/nanomsgcpp/client_pair.cpp
+++ example/nanomsgcpp/client_pair.cpp
@@ -6,23 +6,25 @@
 #include <unistd.h>
 using namespace std;
 
-#define ADDRESS1 "inproc://test"
-#define ADDRESS2 "tcp://127.0.0.1:8000"
-#define ADDRESS3 "ipc:///tmp/reqrep.ipc"
+#define SOCKET_ADDRESS1 "inproc://test"
+#define SOCKET_ADDRESS2 "tcp://47.105.111.100:8000"
+#define SOCKET_ADDRESS3 "tcp://*:8000"
+#define SOCKET_ADDRESS4 "ipc:///tmp/pair.ipc"
 
 int main ()
 {
     nn::socket s2 (AF_SP, NN_PAIR);
-    s2.connect(ADDRESS3)
+    s2.connect(SOCKET_ADDRESS2)
     char buf[25];
 
     s2.send ("...CCC,client send\n", 19, 0);
-    int rc = s2.recv (buf, sizeof (buf), 0);
-    cout<<"...buf,client recv: " << buf << endl;
+    // int rc = s2.recv (buf, sizeof (buf), 0);
+    // cout<<"...buf,client recv: " << buf << endl;
 
     while(1)
     {
-        sleep(1);
+        s2.send ("...CCC,client send\n", 19, 0);
+        sleep(2);
     };
     return 0;
 }
--- example/nanomsgcpp/server_pair.cpp
+++ example/nanomsgcpp/server_pair.cpp
@@ -5,15 +5,16 @@
 #include <iostream>
 #include <unistd.h>
 using namespace std;
-#define ADDRESS2 "ipc:///tmp/reqrep.ipc"
+
 #define SOCKET_ADDRESS1 "inproc://test"
 #define SOCKET_ADDRESS2 "tcp://192.168.1.36:7766"
-#define SOCKET_ADDRESS3 "tcp://*:7766"
+#define SOCKET_ADDRESS3 "tcp://*:8000"
+#define SOCKET_ADDRESS4 "ipc:///tmp/pair.ipc"
 
 int main ()
 {
     nn::socket s1 (AF_SP, NN_PAIR);
-    s1.bind (ADDRESS2);
+    s1.bind (SOCKET_ADDRESS3);
 
     char buf [25];
     while(1)
@@ -21,7 +22,7 @@ int main ()
         int rc = s1.recv (buf, sizeof (buf), 0);
         cout<<"...buf,server recv: " << buf << endl;
         sleep(1);
-        s1.send ("...SSS,server send\n", 19, 0);
+        // s1.send ("...SSS,server send\n", 19, 0);
     };
     return 0;
 }
